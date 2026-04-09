c14cabdbdb5182264516382c825a7c8757a13ef6
Sohan Naidu <sohannaidu123@gmail.com>
2026-04-09 02:21:29 -0500

    [cli] add clear cmd and add memory args for generate

--- cli/src/main/scala/Main.scala
+++ cli/src/main/scala/Main.scala
@@ -1,55 +1,109 @@
 import com.monovore.decline.*
+
 import scala.sys.process.*
 import cats.syntax.all.*
+import com.typesafe.scalalogging.LazyLogging
+
+import java.nio.file.{Files, Path, Paths}
+import scala.jdk.CollectionConverters.*
 
 sealed trait Command
 object Command {
-  final case class Generate(configPath: String, jarPath: String) extends Command
+  final case class Generate(configPath: String, jarPath: String,
+                            minimumMemory: Int, maximumMemory: Int, clearFlag: Boolean) extends Command
 }
 
 object Main extends CommandApp(
   name = "simulator",
   header = "Distributed Algorithms Simulator",
-  main = {
-    val generatorConfig: Opts[String] =
-      Opts.option[String](
-        "generator-config",
-        help = "Path to generator config"
-      ).withDefault("configs/generator.conf")
+  main = CommandLineInterface.run
+)
 
-    val generatorJarPath: Opts[String] =
-      Opts.option[String](
-        "generator-jar",
-        help = "Path to NetGameSim jar file"
-      ).withDefault("core/generator/target/scala-3.2.2/netmodelsim.jar")
+object CommandLineInterface extends LazyLogging {
+  private val clearFlag: Opts[Boolean] =
+    Opts.flag(
+      "clear",
+      help = "Clear the output directory"
+    ).orFalse
 
-    val generate: Opts[Command] =
-      Opts.subcommand("generate", "Generate a graph using NetGameSim") {
-        (generatorConfig, generatorJarPath).mapN(Command.Generate.apply)
+  private val generatorConfig: Opts[String] =
+    Opts.option[String](
+      "config",
+      help = "Path to generator config"
+    ).withDefault("configs/generator.conf")
+
+  private val generatorJarPath: Opts[String] =
+    Opts.option[String](
+      "jar",
+      help = "Path to NetGameSim jar file"
+    ).withDefault("core/generator/target/scala-3.2.2/netmodelsim.jar")
+
+  private val minimumMemory: Opts[Int] =
+    Opts.option[Int](
+      "min",
+      help = "Minimum memory allocation (in GB)"
+    ).withDefault(2)
+
+  private val maximumMemory: Opts[Int] =
+    Opts.option[Int](
+      "max",
+      help = "Maximum memory allocation (in GB)"
+    ).withDefault(4)
+
+  val generate: Opts[Command] =
+    Opts.subcommand("generate", "Generate a graph using NetGameSim") {
+      (generatorConfig, generatorJarPath, minimumMemory, maximumMemory, clearFlag)
+        .mapN(Command.Generate.apply)
+    }
+
+  private val command: Opts[Command] =
+    generate
+
+  def run: Opts[Unit] = command.map {
+    case Command.Generate(configPath, jarPath, minimumMemory, maximumMemory, clearFlag) =>
+      if (clearFlag) {
+        clear()
+      }
+      logger.info(s"Running generator with params config=$configPath, jar=$jarPath, memory alloc between " +
+        s"${minimumMemory}GB and ${maximumMemory}GB")
+      val root = sys.props("user.dir")
+      val exitCode =
+        Process(
+          Seq(
+            "java",
+            s"-Xms${minimumMemory}G",
+            s"-Xmx${maximumMemory}G",
+            s"-Dconfig.file=$configPath",
+            "-jar",
+            s"$jarPath",
+            "generated"
+          )
+        ).!
+
+      if (exitCode != 0) {
+        logger.error(s"Generator failed with exit code $exitCode")
+      }
+  }
+
+  private def clear(): Unit = {
+    val pathStr = "output"
+    val path = Paths.get(pathStr).toAbsolutePath.normalize()
+
+    require(pathStr.nonEmpty && pathStr != "/" && pathStr != ".",
+      s"Refusing to delete unsafe path: $pathStr")
+
+    if (Files.exists(path)) {
+      val stream = Files.walk(path)
+      try {
+        stream.iterator()
+          .asScala
+          .filter(Files.isRegularFile(_))
+          .foreach(Files.delete)
+      } finally {
+        stream.close()
       }
 
-    val command: Opts[Command] =
-      generate
-
-    command.map {
-      case Command.Generate(configPath, jarPath) =>
-        val root = sys.props("user.dir")
-        val exitCode =
-          Process(
-            Seq(
-              "java",
-              "-Xms2G",
-              "-Xmx4G",
-              s"-Dconfig.file=$configPath",
-              "-jar",
-              s"$jarPath",
-              "generated"
-            )
-          ).!
-
-        if (exitCode != 0) {
-          System.err.println(s"Generator failed with exit code $exitCode")
-        }
+      logger.info(s"Cleared directory: $path")
     }
   }
-)
+}
